05a8880d84988e8058d845fee7745c446570e0b5
Stephen Chen <stephenchen0@gmail.com>
2012-06-01 16:21:58 -0400

    Parsed peersFile

--- peer.py
+++ peer.py
@@ -8,35 +8,43 @@ MAX_PEERS = 6
 MAX_FILES = 100
 
 
-class Peer(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
+class Peer:
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('', 50037))
+        s.bind((address, port))
         s.listen(1)
-        conn, addr = s.accept()
-        print 'Connected by', addr
-        while 1:
-            data = conn.recv(1024)
-            if not data: break
-            print 'Server received', repr(data)
-            conn.sendall(data)
-            conn.close()
+        #start accepting connections on listening thread
+        
+    def listen(self):
+        while True:
+            conn, addr = s.accept()
+            
+
+    #===========================================================================
+    # def run(self):
+    #    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #    s.bind(('', 50037))
+    #    s.listen(1)
+    #    conn, addr = s.accept()
+    #    print 'Connected by', addr
+    #    while 1:
+    #        data = conn.recv(1024)
+    #        if not data: break
+    #        conn.sendall(data)
+    #    conn.close()
+    #===========================================================================
 
 
-class Peers(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('localhost', 50037))
-        s.sendall('Hello, world')
-        data = s.recv(1024)
-        s.close()
-        print 'Received', repr(data)
+class Peers:
+    
+    def initialize(self, peersFile):
+        peersFile = "128.81.1.4 58293\n129.2.1.4 58273\n121.14.2.1 22741"
+        file = peersFile.split("\n")
+        for line in file:
+            peerInfo = line.split(" ")
+            peer = Peer(peerInfo[0], peerInfo[1])
 
 
 class Status:
@@ -46,8 +54,5 @@ class Status:
     minimumReplicationLevel = -1
     averageReplicationLevel = -1
 
-peer = Peer()
-peer.start()
-
-peers = Peers()
-peers.start()
+p = Peers()
+p.initialize("")
