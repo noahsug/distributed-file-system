7744216cceb6131a227202913600a0b468220f80
Stephen Chen <stephenchen0@gmail.com>
2012-06-01 16:32:58 -0400

    Added File Class, implemented basic API

--- peer.py
+++ peer.py
@@ -7,20 +7,38 @@ CHUNK_SIZE = 65536
 MAX_PEERS = 6
 MAX_FILES = 100
 
+class File:
+    name_ = ''
+    localChunks_ = 0
+    totalChunks_ = 0
 
 class Peer:
     def __init__(self, address, port):
         self.address = address
         self.port = port
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((address, port))
-        s.listen(1)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((address, port))
+        self.socket.listen(1)
         #start accepting connections on listening thread
         
     def listen(self):
         while True:
-            conn, addr = s.accept()
+            conn, addr = self.socket.accept()
             
+    def join(self):
+        pass
+        
+    def insert(self, fileName):
+        pass
+        
+    def query(self, status):
+        pass
+    
+    def leave(self):
+        pass
+    
+    def worker(self):
+        pass
 
     #===========================================================================
     # def run(self):
@@ -40,19 +58,22 @@ class Peer:
 class Peers:
     
     def initialize(self, peersFile):
-        peersFile = "128.81.1.4 58293\n129.2.1.4 58273\n121.14.2.1 22741"
-        file = peersFile.split("\n")
+        #peersFile = "128.81.1.4 58293\n129.2.1.4 58273\n121.14.2.1 22741"
+        file = peersFile.split('\n')
         for line in file:
-            peerInfo = line.split(" ")
+            peerInfo = line.split(' ')
             peer = Peer(peerInfo[0], peerInfo[1])
 
 
 class Status:
-    numFiles = -1
-    fractionPresentLocally = -1
-    fractionPresent = -1
-    minimumReplicationLevel = -1
-    averageReplicationLevel = -1
+    numFiles_ = -1
+    fractionPresentLocally_ = -1
+    fractionPresent_ = -1
+    minimumReplicationLevel_ = -1
+    averageReplicationLevel_ = -1
+    
+    
+    
 
 p = Peers()
 p.initialize("")
