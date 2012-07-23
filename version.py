29bd36b87a888fdc83c94b2aadd7ad742beb21c0
Noah Sugarman <zetalmac@gmail.com>
2012-07-22 20:40:35 -0400

    added version.py

---
+++ version.py
@@ -0,0 +1,10 @@
+##
+# Represents the version of a file
+##
+
+class Version:
+    def __init__(self):
+        self.fileName = ''
+        self.lastEdited = ''
+        self.numEdits = 1
+        self.numChunks = 0
